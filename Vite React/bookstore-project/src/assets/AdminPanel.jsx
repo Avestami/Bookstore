033739a5841c0579dd2fac3a423611e7449dd337
Avesta.mi <avestanabizadeh@gmail.com>
2024-04-12 02:46:51 +0330

    serverside

---
+++ Vite React/bookstore-project/src/assets/AdminPanel.jsx
@@ -0,0 +1,16 @@
+// AdminPanel.jsx
+import React from 'react';
+
+const AdminPanel = () => {
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <div className="dashboard-content">
+                {/* Add your dashboard content here */}
+                <p>Welcome to your dashboard!</p>
+            </div>
+        </div>
+    );
+};
+
+export default AdminPanel;
